37fa900b4df7294c018263437e641beda9c85dbc
emzaro731-byte <emzaro731@gmail.com>
2026-09-13 12:37:44 +0100

    Delete existing project files

--- src/communityNavigation.ts
+++
@@ -1,4 +0,0 @@
-type Listener=()=>void;
-let listener:Listener|null=null;
-export const onOpenCommunity=(next:Listener)=>{listener=next;return()=>{if(listener===next)listener=null}};
-export const openCommunity=()=>{listener?.()};
