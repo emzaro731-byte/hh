d39edc9f09113b9589b65c4fdc9f022d001ff41e
emzaro731-byte <emzaro731@gmail.com>
2026-09-13 12:36:24 +0100

    Delete existing project files

--- flutter_app/lib/home_dashboard.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'premium_home_page.dart';
-
-class HomeDashboard extends StatelessWidget {
-  const HomeDashboard({super.key});
-
-  @override
-  Widget build(BuildContext context) => const PremiumHomePage();
-}
